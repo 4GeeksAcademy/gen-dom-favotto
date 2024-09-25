baf7bb886f9da1950793ccc194869bc3b451136c
Aless <titifavotto@gmail.com>
2024-09-25 18:59:07 +0000

    mejorado

--- src/app.js
+++ src/app.js
@@ -1,28 +1,44 @@
-Dominio respaldo codigo
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let pronoun = ["the", "my"];
-  let adjective = ["great", "small", "funny"];
-  let noun = ["computer", "dog"];
-  let extension = [".com", ".net", ".es"];
-
-  let domains = [];
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adjective.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < extension.length; l++) {
-          let domainGenerator =
-            pronoun[i] + adjective[j] + noun[k] + extension[l];
-          console.log(domainGenerator);
-        }
-      }
-    }
-  }
+function generateDomainNames(pronouns, adjectives, nouns, extensionsMap) {
+  return pronouns.reduce(
+    (domains, pronoun) =>
+      domains.concat(
+        adjectives.flatMap(adjective =>
+          nouns.flatMap(noun => {
+            const extension = extensionsMap[noun] || ".com"; // Valor por defecto si no encuentra una extensión
+            return [pronoun + adjective + noun + extension];
+          })
+        )
+      ),
+    []
+  );
 }
+
+window.onload = function() {
+  const pronouns = ["the", "my"];
+  const adjectives = ["great", "small", "funny"];
+  const nouns = ["computer", "dog", "camion", "toma", "computado"];
+
+  // Mapeamos cada sustantivo a su extensión correspondiente
+  const extensionsMap = {
+    camion: ".es",
+    toma: ".te",
+    computado: ".ra"
+  };
+
+  const generatedDomains = generateDomainNames(
+    pronouns,
+    adjectives,
+    nouns,
+    extensionsMap
+  );
+
+  for (const domain of generatedDomains) {
+    console.log(domain);
+  }
+};
